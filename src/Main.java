eebdae2d43fa3ae5aac627573ed3066d6defd624
IvanLysenko14 <119072175+IvanLysenko14@users.noreply.github.com>
2022-11-26 03:42:46 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world. Lysenko Ivan 2 lab");
+        System.out.println("Change in web");
     }
 }
